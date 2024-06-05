353d282881ca59377b38dddad392e00d2d082e9a
Pulkit Singh <pulkitsingh01@gmail.com>
2024-06-05 22:17:59 +0530

    error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import FileContent from './components/FileContent';
 import MultiSelectCheckbox from './components/FileTree';
@@ -10,6 +10,33 @@ import { NodeId } from 'react-accessible-treeview';
 
 const queryClient = new QueryClient()
 
+// Error Boundary Component
+class ErrorBoundary extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: any, errorInfo: any) {
+    // You can also log the error to an error reporting service
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      console.error('Something went wrong...');
+    }
+
+    // Normally, just render the children
+    return this.props.children;
+  }
+}
+
 function App() {
   const [selectedFile, setSelectedFile] = useState<{
     nodeId: NodeId;
@@ -17,10 +44,12 @@ function App() {
   }>();
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <MultiSelectCheckbox selectedFile={selectedFile} setSelectedFile={setSelectedFile}/>
-        {selectedFile && <FileContent filePath={selectedFile.filePath} />}
-      </div>
+      <ErrorBoundary> {/* Wrap the entire app in ErrorBoundary */}
+        <div className="App">
+          <MultiSelectCheckbox selectedFile={selectedFile} setSelectedFile={setSelectedFile}/>
+          {selectedFile && <FileContent filePath={selectedFile.filePath} />}
+        </div>
+      </ErrorBoundary>
     </QueryClientProvider>
   );
 }
