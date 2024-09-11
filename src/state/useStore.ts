3cb1451ae527bfc3b230eaa073d73fc20f81f7bf
Pulkit Singh <pulkitsingh01@gmail.com>
2024-09-11 07:05:14 +0530

    useSelector

--- src/state/useStore.ts
+++ src/state/useStore.ts
@@ -1,42 +1,15 @@
 import { useEffect, useState } from "react";
 import { Store } from "./store";
 
-type KeyPaths<T> = T extends object
-  ? {
-      [K in keyof Required<T>]: `${Exclude<K, symbol>}${
-        | ""
-        | (Required<T>[K] extends Array<infer U>
-            ? ""
-            : Required<T>[K] extends object
-            ? `.${KeyPaths<Required<T>[K]>}`
-            : "")}`;
-    }[keyof T]
-  : "";
-
-// Helper function to get nested value
-const getNestedValue = <T>(obj: T, path: string): any => {
-  return path
-    .split(".")
-    .reduce((acc: any, part: string) => acc && acc[part], obj);
-};
-
-const useStore = <S, A>(
+export const useSelector = <S, A, R>(
   subject: Store<S, A>,
-  selector?: Array<KeyPaths<S>> | ((state: S) => any)
-): S => {
+  selector: (state: S) => R
+) => {
   const [state, setState] = useState<S>(subject.getValue());
 
   useEffect(() => {
     const subscription = subject.subscribe((newState) => {
-      const isAnyKeyChanged =
-        typeof selector === "function"
-          ? selector(newState) !== selector(state)
-          : selector?.some((key) => {
-              const oldValue = getNestedValue(state, key as string);
-              const newValue = getNestedValue(newState, key as string);
-              return oldValue !== newValue;
-            });
-      if (isAnyKeyChanged || !selector) {
+      if (selector(newState) !== selector(state)) {
         setState(newState);
       }
     });
@@ -44,7 +17,11 @@ const useStore = <S, A>(
     return () => subscription.unsubscribe();
   }, [subject, selector]);
 
-  return state;
+  return selector(state);
+};
+
+const useStore = <S, A>(subject: Store<S, A>) => {
+  return useSelector(subject, (state) => state);
 };
 
 export default useStore;
